5efc246c234751e681bb4554cc6c4ab34ba7391b
AndnixSH <40742924+AndnixSH@users.noreply.github.com>
2021-04-10 15:56:41 +0200

    Add files via upload

---
+++ HexPatcher/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HexPatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stream outStream = File.Open("libil2cpp.so", FileMode.Open);
+            outStream.Seek(0x100000, SeekOrigin.Begin);
+            outStream.WriteByte(0x1E);
+            outStream.WriteByte(0xFF);
+            outStream.WriteByte(0x2F);
+            outStream.WriteByte(0xE1);
+
+            outStream.Seek(0x123456, SeekOrigin.Begin);
+            outStream.WriteByte(0x00);
+            outStream.WriteByte(0x00);
+            outStream.WriteByte(0xA0);
+            outStream.WriteByte(0xE3);
+            outStream.WriteByte(0x1E);
+            outStream.WriteByte(0xFF);
+            outStream.WriteByte(0x2F);
+            outStream.WriteByte(0xE1);
+            outStream.Close();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HexPatcher
+Simple way to patch hex to the target file in .NET C#
